627bc213dfe3f7a36795aa77cbafe419672f49a4
mauricirododendro <aseretairam1960@libero.it>
2020-03-09 21:21:10 +0100

    Add files via upload
    funzione  e istruzione condizionale, nonchè commenti

---
+++ def_commenti.py
@@ -0,0 +1,10 @@
+f=5
+a=3
+def check(var):
+    print (type(var))
+    if f>a:
+        print ("f maggiore di a")
+    else:
+        print ("f minore o uguale ad a")
+check(0.5)   # f maggiore di a
+             #<class 'float'>
