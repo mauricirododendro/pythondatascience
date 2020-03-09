588de67734eafca0698045ad27e6a9b040e97482
mauricirododendro <aseretairam1960@libero.it>
2020-03-09 20:59:46 +0100

    Add files via upload

---
+++ nomecognome_commenti.py
@@ -0,0 +1,9 @@
+nome="Pippo"
+print (len(nome))  #5
+cognome="Pluto"
+print (nome,cognome)      #Pippo Pluto
+print (nome,' ',cognome)  #Pippo  Pluto
+tot=nome + ' '+cognome
+print (tot)               #Pippo Pluto
+print (tot.upper())       #PIPPO PLUTO
+print (tot)               #Pippo Pluto
